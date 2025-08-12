64de0dd3ebee1c17ae94c55b83b98dfb9d575f4e
AnunayG05 <anunay.gandhi015@nmims.edu.in>
2025-08-12 18:32:49 +0530

    few changes (titles, etc)

--- src/components/sections/what-we-do-section.tsx
+++ src/components/sections/what-we-do-section.tsx
@@ -4,18 +4,8 @@ import { Lightbulb, Rocket, Users } from "lucide-react";
 const activities = [
     {
         icon: <Rocket className="w-10 h-10 text-accent" />,
-        title: "Lorem Ipsum",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolor reiciendis aperiam laboriosam assumenda, neque veritatis eos voluptatem saepe corporis sunt delectus provident qui, quam dolore! Quidem natus architecto tempora?."
-    },
-    {
-        icon: <Lightbulb className="w-10 h-10 text-accent" />,
-        title: "Lorem Ipsum 2",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolor reiciendis aperiam laboriosam assumenda, neque veritatis eos voluptatem saepe corporis sunt delectus provident qui, quam dolore! Quidem natus architecto tempora?."
-    },
-    {
-        icon: <Users className="w-10 h-10 text-accent" />,
-        title: "Lorem Ipsum 3",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolor reiciendis aperiam laboriosam assumenda, neque veritatis eos voluptatem saepe corporis sunt delectus provident qui, quam dolore! Quidem natus architecto tempora?."
+        title: "Events",
+        description: "We focus on events that inculcate enjoyment as well as contain a generous dose of Marketing related tasks. We also organize workshops and seminars which are respected by industry leaders, academicians and participants alike. In this competitive world, making a lasting impression is important- now more than ever. We believe in helping students learn to hone their skills in this regard so that nothing stops them from achieving their goals!"
     }
 ]
 
